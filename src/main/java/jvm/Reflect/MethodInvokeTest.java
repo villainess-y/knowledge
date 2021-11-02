edf7190fc23cf7bc7e9d931085b23b99bc310b14
villainess-y <1090736414@qq.com>
2021-11-02 20:53:11 +0800

    [FIX]modify .gitignore

--- src/main/java/jvm/Reflect/MethodInvokeTest.java
+++ src/main/java/jvm/Reflect/MethodInvokeTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package jvm.Reflect;
 
 import java.lang.reflect.Method;
@@ -16,22 +15,3 @@ public class MethodInvokeTest {
         System.out.println("test: " + i);
     }
 }
-=======
-package jvm.Reflect;
-
-import java.lang.reflect.Method;
-
-public class MethodInvokeTest {
-    public static void main(String[] args) throws Exception {
-        Class<?> clazz = Class.forName("jvm.Reflect.MethodInvokeTest");
-        Method method = clazz.getMethod("target", int.class);
-        for (int i = 0; i < 16; i++) {
-            method.invoke(null, i);
-        }
-    }
-
-    public static void target(int i) {
-        System.out.println("test: " + i);
-    }
-}
->>>>>>> 9eb1b2bf315f0fcfb8bfd8d990752d3280a5cbb2
